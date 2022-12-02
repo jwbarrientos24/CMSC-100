c6d909f43b7a34b3d39088d6b3c937949b8fd056
jwbarrientos24 <jwbarrientos@up.edu.ph>
2022-12-02 07:29:51 +0000

    chore: added src/index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Fastify from 'fastify';
+
+async function start() {
+    try {
+        const fastify
+
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+
+}
+
+start();
