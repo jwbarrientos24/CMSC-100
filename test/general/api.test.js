7397a17a47caf64414f9fe7a697d9edc6645f443
jwbarrientos24 <jwbarrientos@up.edu.ph>
2022-12-02 13:57:46 +0000

    chore: added api.test.js for testing

---
+++ test/general/api.test.js
@@ -0,0 +1,25 @@
+import tap from 'tap';
+import { build } from '../../src/app.js';
+import 'must/register.js';
+
+tap.mochaGlobals();
+
+describe('/api should not work', async () => {
+    let app;
+
+    before(async() => {
+        app = await build()
+    });
+
+    it('should return { success: true }', async() => {
+        const response = await app.inject({
+            method: 'GET',
+            url: '/api'
+        })
+        response.statusCode.must.be.equal(200);
+        const result = await response.json();
+        response.success.must.be.true();
+    })
+
+
+} )
