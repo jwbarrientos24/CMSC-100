fece78ad3a02b6507c173d0f4c8d475a1fb4f2cd
jwbarrientos24 <jwbarrientos@up.edu.ph>
2023-01-03 07:37:53 +0000

    chore: refactor to avoid clutter

--- src/app.js
+++ src/app.js
@@ -1,38 +1,13 @@
 import Fastify from 'fastify';
-import { getDB, saveDB } from './utils/db/index.js';
-import { v4 } from 'uuid';
+import { createTodo } from './services/todos/create-todo.js';
+import { general } from './services/general/index.js';
 
 const prefix = '/api';
 
 export async function build () {
   const fastify = Fastify({ logger: true });
-  fastify.get(prefix, async (request, reply) => {
-    return { success: true };
-  });
+  fastify.get(prefix, general);
 
   // create todo
-  fastify.post(`${prefix}/todo`, async (request, reply) => {
-    const { body } = request;
-    const { title, description, isDone = false } = body;
-    const db = await getDB();
-
-    const id = v4();
-
-    const todo = {
-      title,
-      description,
-      isDone,
-      createdDate: new Date().getTime(),
-      updatedDate: new Date().getTime()
-    };
-
-    db.todos[id] = todo;
-
-    await saveDB(db);
-
-    return {
-      id,
-      ...todo
-    };
-  });
+  fastify.post(`${prefix}/todo`, createTodo);
 }
